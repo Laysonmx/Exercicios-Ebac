505ae2fbda178c0447688c978423b2a20df34b3d
Laysonmx <imsecondaryl@gmail.com>
2025-04-17 14:55:48 -0300

    Notas de Aluno

---
+++ Notasaluno.java
@@ -0,0 +1,19 @@
+package mod10;
+
+public class Notasaluno {
+
+    public static void main(String args[]) {
+
+        int result = 11;
+
+        if (result > -1 && result < 5) {
+            System.out.println("Reprovado");
+        } else if (result >=5 && result < 7) {
+            System.out.println("Recuperação"); {
+            }
+    } else if (result >=7 && result < 11){
+            System.out.println("Aprovado");
+        }
+        }
+
+}
